8ccb2dd6ac333b5d348d53a1d098e6b3f9529da8
mishthygupta <mishthygupta2005@gmail.com>
2026-01-03 16:18:13 +0530

    Fix MongoStore initialization for connect-mongo

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const store = MongoStore.create({
+const store = new MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
     secret: process.env.SECRET,
